5354d2a3f4cc29deacb4c89dd8332d3774e1255a
Kamil Zawadzki <kamil.zawadzki96@outlook.com>
2021-12-21 09:53:23 +0100

    Docker

--- DockerInstall.sh
+++ DockerInstall.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install \
+sudo apt-get -y update
+sudo apt-get -y install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --batch --yes /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
