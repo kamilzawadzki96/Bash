a030bb42a10f18472019345568c752042b7cc5b6
Kamil Zawadzki <kamil.zawadzki96@outlook.com>
2021-12-21 10:18:20 +0100

    NPM

--- DockerInstall.sh
+++ DockerInstall.sh
@@ -11,4 +11,4 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get -y update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose
