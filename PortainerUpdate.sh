7baf6a6c5e4416a09c8d497fa0f3e918c154e4e1
Kamil Zawadzki <kamil.zawadzki96@outlook.com>
2021-12-22 11:20:32 +0100

    Portainer Update

--- PortainerUpdate.sh
+++ PortainerUpdate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo docker stop portainer
-sudo docker rm portainer
-sudo docker pull cr.portainer.io/portainer/portainer-ce:latest
+docker stop portainer
+docker rm portainer
+docker pull cr.portainer.io/portainer/portainer-ce:latest
 
-sudo docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 \
+docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 \
     --name=portainer --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
